19888d4a75c35038f7c14ea2f3a7bd52dad28e69
dpaukov <d.paukov@gmail.com>
2020-09-27 00:07:11 -0700

    Binary Tree Traversal: in-order, pre-order, post-order, and bfs algorithms (recursive and iterative).

---
+++ src/main/java/org/paukov/tree/BinaryTreeTraversal.java
@@ -0,0 +1,151 @@
+package org.paukov.tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class BinaryTreeTraversal {
+
+  /*================ Recursive traversals ===================*/
+
+  static void inorder(TreeNode root, List<Integer> output) {
+    if (root == null) {
+      return;
+    }
+    inorder(root.left, output);
+    output.add(root.val);
+    inorder(root.right, output);
+  }
+
+  static void preorder(TreeNode root, List<Integer> output) {
+    if (root == null) {
+      return;
+    }
+    output.add(root.val);
+    preorder(root.left, output);
+    preorder(root.right, output);
+  }
+
+  static void postorder(TreeNode root, List<Integer> output) {
+    if (root == null) {
+      return;
+    }
+    postorder(root.left, output);
+    postorder(root.right, output);
+    output.add(root.val);
+  }
+
+  /*================ Iterative traversals ===================*/
+
+  static void bfs(TreeNode root, List<Integer> output) {
+    if (root == null) {
+      return;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+      output.add(node.val);
+      if (node.left != null) {
+        queue.add(node.left);
+      }
+      if (node.right != null) {
+        queue.add(node.right);
+      }
+    }
+  }
+
+  static void inorderIterative(TreeNode root, List<Integer> output) {
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode node = root;
+
+    while (node != null || !stack.isEmpty()) {
+      while (node != null) {
+        stack.add(node);
+        node = node.left;
+      }
+      node = stack.pop();
+      output.add(node.val);
+      node = node.right;
+    }
+  }
+
+  static void preorderIterative(TreeNode root, List<Integer> output) {
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.pop();
+      output.add(node.val);
+      // We need to put the right node first to the stack so the left node
+      // will be on the top of the stack.
+      if (node.right != null) {
+        stack.push(node.right);
+      }
+      if (node.left != null) {
+        stack.push(node.left);
+      }
+    }
+  }
+
+  static void postorderIterative2Stacks(TreeNode root, List<Integer> output) {
+    Stack<TreeNode> stack1 = new Stack<>();
+    Stack<TreeNode> stack2 = new Stack<>();
+    stack1.push(root);
+
+    while (!stack1.isEmpty()) {
+      TreeNode node = stack1.pop();
+      stack2.push(node);
+      if (node.left != null) {
+        stack1.push(node.left);
+      }
+      if (node.right != null) {
+        stack1.push(node.right);
+      }
+    }
+
+    // the second stack contains the result in the reverse order.
+    while (!stack2.isEmpty()) {
+      output.add(stack2.pop().val);
+    }
+  }
+
+  // https://www.youtube.com/watch?v=xLQKdq0Ffjg
+  static void postorderIterative1Stack(TreeNode root, List<Integer> output) {
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode current = root;
+
+    while (!stack.isEmpty() || current != null) {
+      while (current != null) {
+        stack.push(current);
+        current = current.left;
+      }
+
+      if (stack.peek().right != null) {
+        current = stack.peek().right;
+        continue;
+      }
+
+      TreeNode node = stack.pop();
+      output.add(node.val);
+
+      while (!stack.isEmpty() && node == stack.peek().right) {
+        node = stack.pop();
+        output.add(node.val);
+      }
+    }
+  }
+
+  public static class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+}
---
+++ src/test/java/org/paukov/tree/BinaryTreeTraversalTest.java
@@ -0,0 +1,103 @@
+package org.paukov.tree;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.paukov.tree.BinaryTreeTraversal.TreeNode;
+
+final class BinaryTreeTraversalTest {
+
+  TreeNode createTestTree() {
+    TreeNode root = new TreeNode(5);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(7);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(8);
+    root.left.left = new TreeNode(2);
+    root.left.right = new TreeNode(4);
+    root.left.left.left = new TreeNode(1);
+    return root;
+  }
+
+  @Test
+  void inorder() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.inorder(root, expected);
+
+    assertThat(expected).containsExactly(1, 2, 3, 4, 5, 6, 7, 8);
+  }
+
+  @Test
+  void preorder() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.preorder(root, expected);
+
+    assertThat(expected).containsExactly(5, 3, 2, 1, 4, 7, 6, 8);
+  }
+
+  @Test
+  void postorder() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.postorder(root, expected);
+
+    assertThat(expected).containsExactly(1, 2, 4, 3, 6, 8, 7, 5);
+  }
+
+  @Test
+  void inorderIterative() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.inorderIterative(root, expected);
+
+    assertThat(expected).containsExactly(1, 2, 3, 4, 5, 6, 7, 8);
+  }
+
+  @Test
+  void preorderIterative() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.preorderIterative(root, expected);
+
+    assertThat(expected).containsExactly(5, 3, 2, 1, 4, 7, 6, 8);
+  }
+
+  @Test
+  void postorderIterative2Stacks() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.postorderIterative2Stacks(root, expected);
+
+    assertThat(expected).containsExactly(1, 2, 4, 3, 6, 8, 7, 5);
+  }
+
+  @Test
+  void postorderIterative1Stack() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.postorderIterative1Stack(root, expected);
+
+    assertThat(expected).containsExactly(1, 2, 4, 3, 6, 8, 7, 5);
+  }
+
+  @Test
+  void bfs() {
+    TreeNode root = createTestTree();
+
+    List<Integer> expected = new ArrayList<>();
+    BinaryTreeTraversal.bfs(root, expected);
+
+    assertThat(expected).containsExactly(5, 3, 7, 2, 4, 6, 8, 1);
+  }
+}
